6f01495ade6c5a67bcc2cf09625fde06f2e7622f
y-tsutsumi <tsutsu0928@gmail.com>
2014-06-01 17:57:03 +0900

    テンプレートエンジンを使って，Viewを作成．

---
+++ DjangoProject/polls/urls.py
@@ -0,0 +1,14 @@
+﻿# coding: utf-8
+
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('polls.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+)

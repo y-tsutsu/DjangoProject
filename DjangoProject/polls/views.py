0c31a193fc94123d071ecef60212d615d40cf488
y-tsutsumi <tsutsu0928@gmail.com>
2014-06-04 01:52:36 +0900

    投票フォームと結果表示フォームを作成．

--- DjangoProject/polls/views.py
+++ DjangoProject/polls/views.py
@@ -1,18 +1,21 @@
 ﻿# coding: utf-8
 
 from django.shortcuts import render_to_response, get_object_or_404
-from polls.models import Poll
-
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by("-pub_date")[:5]
-    return render_to_response("polls/index.html", {"latest_poll_list": latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk = poll_id)
-    return render_to_response("polls/detail.html", {"poll": p})
-
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll {0}.".format(poll_id))
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from polls.models import Choice, Poll
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll {0}.".format(poll_id))
+    p = get_object_or_404(Poll, pk = poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk = request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response("polls/detail.html", {
+            "poll": p,
+            "error_message": "選択肢を選んでいません．",
+        }, context_instance = RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("poll_results", args = (p.id,)))
